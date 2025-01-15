7f3e2bbdf1eea3d77af52afc650c0b7ebb84dabc
AllanLps <allanlps20@gmail.com>
2025-01-15 17:26:22 -0300

    enviando do pc da rapha

--- app/page.js
+++ app/page.js
@@ -1,4 +1,6 @@
 import { Button } from "/components/ui/button";
+import { Input } from "/components/ui/input";
+import { Label } from "/components/ui/label";
 import { MoveRight } from 'lucide-react';
 import { Caveat } from "next/font/google";
 import Link from "next/link";
@@ -13,11 +15,11 @@ const caveat = Caveat({
 export default function Home() {
   return (
     <>
-      <section className="grid grid-cols-1 sm:grid-cols-2 min-h-screen">
+      <section className="grid grid-cols-1 sm:grid-cols-2 min-h-screen justify-center">
         {/* Coluna de informações */}
         <section className="bg-blue-500 text-white flex flex-col justify-center items-start p-10">
           <h2 className="text-5xl font-bold mb-4">
-            Boas-vinda ao <span className={`${caveat.className}`}>FinancePlan</span>
+            Bem vindo ao <span className={`${caveat.className}`}>FinancePlan</span>
           </h2>
           <p className="text-2xl mb-6">
             O seu primeiro passo em direção à organização financeira!
@@ -25,46 +27,37 @@ export default function Home() {
         </section>
 
         {/* Coluna de Login */}
-        <section className="flex flex-col justify-center items-start p-24 min-h-screen">
-        <h2 className="text-3xl font-bold text-left mb-4">Sign In</h2>
+        <section className="flex flex-col justify-center items-center m-auto w-full">
+        
 
-        <div className="flex text-left mb-4">
-          <p className="text-gray-600 mr-1 text-sm">É novo por aqui?</p>
-          <Link href="/dashboard" className="text-blue-500 text-sm">Crie sua conta</Link>
+        <div className="flex flex-col items-start text-left mb-4  w-[400px]">
+          <h2 className="text-3xl font-bold text-left mb-4">Sign In</h2>
+          <div className="flex gap-1">
+            <p className="text-gray-600 mr-1 text-sm">É novo por aqui?</p>
+            <Link href="/dashboard" className="text-blue-500 text-sm">Crie sua conta</Link>
+          </div>
+          
         </div>
 
         {/* Formulário de login */}
-        <form className="w-full max-w-xs">
+        <form className="w-[400px]">
           <div className="mb-4">
-            <label htmlFor="email" className="block text-lg font-semibold mb-2 text-left">
-              Email
-            </label>
-            <input
-              id="email"
-              type="email"
-              placeholder="Digite seu email"
-              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
+            <Label htmlFor="email" className="font-bold">E-mail</Label>
+            <Input type="email" id="email" placeholder="E-mail"></Input>
           </div>
 
           <div className="mb-6">
-            <label htmlFor="password" className="block text-lg font-semibold mb-2 text-left">
-              Senha
-            </label>
-            <input
-              id="password"
-              type="password"
-              placeholder="Digite sua senha"
-              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
+            <Label className="font-bold">Senha</Label>
+            <Input type="password" id="password" placeholder="password" ></Input>
+            <Link className="text-gray-500 hover:text-red-500 text-sm mb-1" href="/trocarSenha">Esqueci minha senha</Link>
           </div>
 
-          <button
+          <Button
             type="submit"
-            className="w-full py-3 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600"
+            className=""
           >
             Entrar
-          </button>
+          </Button>
         </form>
       </section>
 
